02689c56d95e13ea3314d5e06e3147f168229467
alex <alex>
2008-01-04 12:46:07 +0000

    update clean and makedist scripts

--- makedist.sh
+++ makedist.sh
@@ -7,43 +7,46 @@ set -e
 
 AR_BIN=i8080emu.zip
 AR_SRC=i8080emu-src.zip
-
 rm -f $AR_BIN $AR_SRC
 
 rm -rf dist-bin
-rm -rf dist-src
+mkdir  dist-bin
 
-mkdir dist-bin
-mkdir dist-src
 
+PLUGINS="src/plg_spkr/speaker.dll src/plg_comport/com_port.dll"
 
 echo "Make binary archive"
 cp src/i8080emu.exe              dist-bin
-cp doc/i8080emu.hlp              dist-bin
+cp doc/I8080EMU.HLP              dist-bin/i8080emu.hlp
 cp doc/i8080emu.cnt              dist-bin
 cp readme.html screenshot.png    dist-bin
 mkdir dist-bin/plugins
-#cp src/plg_spkr/speaker.dll      dist-bin/plugins
-#cp src/plg_comport/com_port.dll  dist-bin/plugins
+cp $PLUGINS                      dist-bin/plugins
 mkdir dist-bin/examples
-cp examples/*                    dist-bin/examples
+cp examples/*.asm                dist-bin/examples
 cp AUTHORS BUGS ChangeLog TODO   dist-bin
 cp COPYING                       dist-bin
 
 (cd dist-bin ; zip -r ../$AR_BIN *)
 
+rm -rf dist-bin
 
 
 echo "Make source archive"
 
-zip $AR_SRC AUTHORS BUGS ChangeLog COPYING doc examples images \
+sh src/clean.sh
+
+zip $AR_SRC AUTHORS BUGS ChangeLog COPYING doc images \
             readme.html screenshot.png TESTING TODO
 
-zip -R $AR_SRC `find -name '*.h' -o -name '*.cpp' -o -name '*.c' -o -name '*.dfm' -o -name '*.bpr' -o -name '*.txt' -o -name '*.bpf'`
+zip -r $AR_SRC src doc examples images -x '*CVS/*' -x '*.exe' -x '*.dll' -x '*.hlp' -x '*~'
 
 echo Visual test distributive:
 echo Libraries use:
-wine tdump.exe src/i8080emu.exe | grep "Imports from"
+wine tdump.exe src/i8080emu.exe  | grep "Imports from"
 
-rm -rf dist-bin
-rm -rf dist-src
+for p in $PLUGINS ; do
+  echo "Imports in $p"
+  wine tdump.exe -em. $p | grep 'IMPORT:'
+done
+
--- src/clean.sh
+++ src/clean.sh
@@ -4,3 +4,4 @@ find -iname '*.~*' -o -iname '*.obj' -o -iname '*.td?' \
   -o -name '*.ddp' -o -name '*.res' | xargs rm -f
 
 rm -f i8080emu.INI
+
