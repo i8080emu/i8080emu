304dee7de6d13c910f0937ef5be8ad1a0fb86ee8
djv <djv>
2008-12-22 07:59:51 +0000

    minor remove Thunbs from windows explorer in clean.sh

--- src/clean.sh
+++ src/clean.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-find -iname '*.~*' -o -iname '*.obj' -o -iname '*.td?' \
+find -iname '*.~*' -o -iname '*.obj' -o -iname '*.td?' -o -name 'Thumbs.db' \
   -o -name '*.ddp' -o -name '*.res' | xargs rm -f
 
 rm -f i8080emu.INI
